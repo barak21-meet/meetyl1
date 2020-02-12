988bc57f74b2c71a8e523919808fc0aae25f88b6
barak21-meet <52946673+barak21-meet@users.noreply.github.com>
2020-02-12 22:34:35 +0200

    Add files via upload

---
+++ main_backend.js
@@ -0,0 +1,11 @@
+//ADD THIS IN THE HTML:  <script type="text/javascript" src="main_backend.js"></script>
+//ADD TO THE FORM AN ACTION AND A METHOD
+//MAKE SURE ALL ID NAMES OF THE TEMPORARY HTML FILE BY CELINE FITS THE SAME ID NAMES IN CELINE'S PAGE
+function first()
+{
+	var time1=document.getElementById("time1").value;
+	localStorage.setItem("time1",document.getElementById("time1").value);
+}
+
+
+
